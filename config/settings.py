ccc86f7f3f617050ea2138da59e2217606042b89
adinaim <adinaitm@gmail.com>
2023-01-26 20:53:23 +0600

    email forgotten

--- config/settings.py
+++ config/settings.py
@@ -209,3 +209,11 @@ LOGGING = {
         },
     },
 }
+
+
+EMAIL_BACKENDS = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST_USER = config('EMAIL_HOST_USER') 
+EMAIL_PORT = config('EMAIL_PORT', default = 587)
+EMAIL_HOST = config('EMAIL_HOST') 
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD') 
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
