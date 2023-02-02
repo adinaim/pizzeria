a8e807c88d53d46ef6fa8e75be0ff3aa6b4470eb
aigerim055 <omurzakova727@gmail.com>
2023-02-02 09:30:46 +0600

    pizza crud

--- apps/pizza/models.py
+++ apps/pizza/models.py
@@ -4,26 +4,30 @@ from django.db import models
 
 class Pizza(models.Model):
 
+    SIZE_CHOISES = (
+            ('30', 30),
+            ('40', 40)
+        )
+
+# (
+# 30, (‘weight’: ‘360 грамм’, ‘price’: 420),
+# 40, (‘weight’: ‘560 грамм’, ‘price’: 620)
+# )
+
     image = models.ImageField(verbose_name='image')
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=10, unique=True)
     desc = models.CharField(max_length=200)
+    size = models.CharField(max_length=100, choices=SIZE_CHOISES)
+    weight = models.CharField(max_length=50, blank=True)
+    price = models.CharField(max_length=50, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+
 
     class Meta:
         verbose_name = 'Пицца'
         verbose_name_plural = 'Пиццы'
     
-
-class ConcretePizza(models.Model):
-
-    pizza = models.ForeignKey(
-        to=Pizza,
-        on_delete=models.CASCADE,
-        related_name='pizza'
-    )
-    weight = models.CharField(max_length=100)
-    size = models.CharField(max_length=100)
-    price = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = 'Конкретная Пицца'
-        verbose_name_plural = 'Конкретные Пиццы'
